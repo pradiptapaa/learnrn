8da350fd680dea3b19d1e8d58c3ad06ad25c1ade
Pradipta Priya Adyatmika <pradiptapaa@gmail.com>
2020-03-23 17:16:21 +0700

    learn-network-request

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -2,15 +2,40 @@ import React, { useState } from 'react'
 import { View, StyleSheet, TextInput, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import SearchBar from '../component/SearchBar';
+import moviedb from '../api/moviedb'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
+    const [result, setResult] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await moviedb.get('/search/movie', {
+                params: {
+                    api_key: '8c3840e4b2d6509ad49c9ba7a78bd582',
+                    query: term
+                }
+            });
+            setResult(response.data.results)
+            setErrorMessage("")
+        } catch (error) {
+            console.log(error)
+            setErrorMessage("Something went wrong ...")
+        }
+    }
+
+    const doSearch = () => {
+        term.length == 0 ? <Text>Movie can't be empty</Text> : searchApi
+    }
     return (
         <View>
-           <SearchBar 
-           term={term} 
-           onTermChange={newTerm => setTerm(newTerm)}/>
-           <Text>{term}</Text>
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi} />
+            {errorMessage.length != 0?<Text style={styles.errorMessage}>{errorMessage}</Text>:null}
+            <Text style={styles.result}>There are {result.length} movies for keyword: {term}</Text>
         </View>
     )
 };
@@ -26,12 +51,20 @@ const styles = StyleSheet.create({
     },
     textInput: {
         flex: 1,
-        fontSize:18
+        fontSize: 18
     },
     searchIcon: {
         fontSize: 35,
         alignSelf: 'center',
-        marginHorizontal:8
+        marginHorizontal: 8
+    },
+    errorMessage:{
+        color:'red',
+        marginHorizontal:15
+
+    },
+    result:{
+        marginHorizontal:15
     }
 })
 
