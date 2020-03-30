81ca9b7730e7769218428611865f87ab44b83924
Pradipta Priya Adyatmika <pradiptapaa@gmail.com>
2020-03-30 18:08:25 +0700

    learn-context-useReduce

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 
 const AppContext = React.createContext();
 
+const blogReducer = (state,action) =>{
+    switch (action.type){
+        case 'add_blogpost':
+            return [...state, {title:`Blog Post #${state.length+1}`}];
+        default:
+            return state;
+    }
+};
+
 export const AppProvider = ({children}) => {
 
-    const [blogPosts,setBlogPosts] = useState([]);
+    // Using useState
+    // const [blogPosts,setBlogPosts] = useState([]);
+    const [blogPosts,dispatch] = useReducer(blogReducer,[]);
 
     const addBlogPost = () =>{
-        setBlogPosts([...blogPosts,{title:`My blog post #${blogPosts.length}`}])
-    }
+        // Using useState
+        // setBlogPosts([...blogPosts,{title:`My blog post #${blogPosts.length+1}`}])
+        dispatch({type:'add_blogpost'});
+    };
 
     return <AppContext.Provider
     value={{data: blogPosts,addBlogPost}}>
