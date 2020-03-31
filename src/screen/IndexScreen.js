46124fa4a34bebfe83ae6c53b0dbb6af6053f40b
Pradipta Priya Adyatmika <pradiptapaa@gmail.com>
2020-03-31 16:30:51 +0700

    learn-deleting-flatlist

--- src/screen/IndexScreen.js
+++ src/screen/IndexScreen.js
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react'
-import { Text, FlatList, View, Button } from 'react-native'
+import { Text, FlatList, View, Button, StyleSheet, TouchableOpacity } from 'react-native'
 // Using normal context ...
 // import AppContext from '../context/AppContext'
 // Using automatic context ....
 import { Context } from '../context/AutomaticAppContext'
+import Icon from 'react-native-vector-icons/Feather';
+
 
 const IndexScreen = () => {
     // Change data to state
     // const {data,addBlogPost} = useContext(Context)
-    const {state,addBlogPost} = useContext(Context)
+    const {state,addBlogPost, deleteBlogPost} = useContext(Context)
 
     return (
         <View>
@@ -17,12 +19,35 @@ const IndexScreen = () => {
             <FlatList
                 // data={data}
                 data={state}
-                keyExtractor={(blogPost) => blogPost.title}
+                keyExtractor={(blogPost) => blogPost.id}
                 renderItem={({ item }) => {
-                    return<Text>{item.title}</Text>}
+                    return <View style={styles.row}>
+                        <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={()=> deleteBlogPost(item.id)}>
+                        <Icon name="trash" style={styles.title}/>
+                        </TouchableOpacity>
+                        </View>
+                    }
                 } />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:20,
+        borderTopWidth:1,
+        borderColor:'gray'
+    },
+    title:{
+        fontSize:18
+    },
+    icon:{
+        fontSize:24
+    }
+
+})
 
 export default IndexScreen;
